304272350ba92f70a980fb648abe9c429b92a6a9
Cpietro87 <cristianpietro2019@gmail.com>
2023-08-30 00:19:13 -0300

    se agreso estilos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,49 +7,56 @@ function App() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const newMessage = {
       body: message,
-      from: 'Me'
-    }
-    setMessages([...messages, newMessage]),
-    socket.emit('message', message)
+      from: "Me",
+    };
+    setMessages([...messages, newMessage]), socket.emit("message", message);
   };
 
   useEffect(() => {
-    socket.on('message',
-      receiveMessage
-    );
+    socket.on("message", receiveMessage);
 
     return () => {
-      socket.off('message', receiveMessage);
-    }
-  },[]);
+      socket.off("message", receiveMessage);
+    };
+  }, []);
 
   const receiveMessage = (message) => {
-    setMessages((state) => [ ...state, message]);
-  }
+    setMessages((state) => [...state, message]);
+  };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2">Chat React</h1>
         <input
           type="text"
           placeholder="Write your messege ..."
+          className="border-2 border-zinc-500 p-2 w-full text-black"
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button>send</button>
+
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, i) => (
+            <li
+              key={i}
+              className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"
+            }`}
+            >
+              <span className="text-xs text-slate-300 block">
+                {message.from}
+              </span>
+              <span className="text-sm">
+
+              {message.body}
+              </span>
+            </li>
+          ))}
+        </ul>
       </form>
-      <ul>
-        {
-          messages.map((message, i) => (
-            <li key={i}>
-              {message.from}:{message.body}</li>
-          ))
-        }
-      </ul>
     </div>
   );
 }
