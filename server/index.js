8f4bfcbf2cd49033f0b303fb1bead4fd1b285814
Cpietro87 <cristianpietro2019@gmail.com>
2023-08-29 22:14:44 -0300

    Configuración del proyecto

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
+io.on('connection', socket =>{
+    console.log('Client connect')
+})
+
+server.listen(3000, () => {
+    console.log('Server on port 3000')
+});
